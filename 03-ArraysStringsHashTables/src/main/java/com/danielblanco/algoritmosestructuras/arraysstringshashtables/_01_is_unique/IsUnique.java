e22af5210d68c1141fdef5b44a1ed5569d8fda50
jhecohe <jhecohe@gmail.com>
2025-02-08 15:37:08 -0500

    03-01 isUnique resuelto

--- 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
+++ 03-ArraysStringsHashTables/src/main/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_01_is_unique/IsUnique.java
@@ -1,5 +1,7 @@
 package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_unique;
 
+import java.util.HashSet;
+
 /*
  * Dado un método que recibe una String, comprobar si todos los caracteres son únicos o no.
  *
@@ -9,6 +11,16 @@ package com.danielblanco.algoritmosestructuras.arraysstringshashtables._01_is_un
 public class IsUnique {
 
   public boolean isUnique(String s) {
-    throw new UnsupportedOperationException("Not implemented yet");
+    if(s.length() > 128 ) return false;
+    HashSet<Character> caracteres = new HashSet<>();
+
+    for (Character c: s.toCharArray()){
+      if (!caracteres.contains(c)) {
+        caracteres.add(c);
+      } else {
+        return false;
+      }
+    }
+    return true;
   }
 }
--- 03-ArraysStringsHashTables/src/test/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSumTest.java
+++ 03-ArraysStringsHashTables/src/test/java/com/danielblanco/algoritmosestructuras/arraysstringshashtables/_02_two_sum/TwoSumTest.java
@@ -10,9 +10,12 @@ public class TwoSumTest {
   @Test
   public void twoSumTest() {
     int[] array = new int[] {9, 2, 5, 6};
+    int[] array2 = new int[] {9, 2, 5, 6, 4};
     TwoSum twoSum = new TwoSum();
-    int[] result = twoSum.twoSum(array, 7);
-    assertTrue((result[0] == 1 && result[1] == 2) || (result[0] == 2 && result[1] == 1));
-    assertEquals(null, twoSum.twoSum(array, 50));
+//    int[] result = twoSum.twoSum(array2, 7);
+//    assertTrue((result[0] == 1 && result[1] == 2) || (result[0] == 2 && result[1] == 1));
+//    assertEquals(null, twoSum.twoSum(array, 50));
+    int[] result2 = twoSum.twoSum(array, 10);
+    assertTrue((result2[0] == 6 && result2[1] == 4) || (result2[0] == 4 && result2[1] == 6));
   }
 }
